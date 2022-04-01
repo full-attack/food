4667c59408e1d9d0254d92393aed2889fe773a38
easy_il <75256407+full-attack@users.noreply.github.com>
2022-04-01 14:21:34 +0300

    Create Misstake.dart

---
+++ Misstake.dart
@@ -0,0 +1,277 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeScreen> {
+  File? imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints:
+            BoxConstraints(minHeight: viewportConstraints.maxHeight),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    'Загрузите фото, на котором видны Ваши продукты. К примеру это может быть фото холодильника',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if (imageFile != null)
+                    Container(
+                      width: 300,
+                      height: 300,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: FileImage(imageFile!), fit: BoxFit.cover),
+                        //border: Border.all(width: 8, color: Colors.black),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    )
+                  else
+                    Container(
+                      width: 300,
+                      height: 300,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color(0xffa7c8fd),
+                        //border: Border.all(width: 8, color: Colors.black12),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: const Text(
+                        'Image should appear here',
+                        style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontSize: 30,
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Palanquin'),
+                      ),
+                    ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                            onPressed: () =>
+                                getImage(source: ImageSource.camera),
+                            child: const Text('Capture Image',
+                                style: TextStyle(fontSize: 18))),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                            onPressed: () =>
+                                getImage(source: ImageSource.gallery),
+                            child: const Text('Select Image',
+                                style: TextStyle(fontSize: 18))),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    child: Text("Перейти к списку рецептов"),
+                    onPressed: () => Upload(imageFile!),
+                    //color: Color(0xff24997f),
+                    //textColor: Colors.white,
+                    //shape: RoundedRectangleBorder(
+                    //borderRadius: BorderRadius.all(Radius.circular(25))),
+                  ),
+                  Text(
+                    'Вывод',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  Text(
+                    'Вывод',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  Text(
+                    'Загрузите фото, на котором видны Ваши продукты. К примеру это может быть фото холодильника',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  Text(
+                    'Вывод',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  Text(
+                    'Вывод',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Palanquin'),
+                  ),
+                  Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 4),
+                      height: 100,
+                      child: Card(
+                          child: FutureBuilder(
+                            future: Upload(imageFile!),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData)
+                             {
+                                return Text('${snapshot.data}');
+                              }
+                                else if (snapshot.error != null) {
+                                return Text('${snapshot.error}');
+                                  }
+                              return const CircularProgressIndicator();
+                            }
+
+                          )
+                      )
+                  )
+                ]
+            ),
+          ),
+          // FutureBuilder(
+          //     future: Upload(imageFile!),
+          //     initialData: 0.0,
+          //     builder: (context, snapshot) {
+          //       if (snapshot.hasData) {
+          //         return Text("Привет");
+          //       } else {
+          //         return Center(
+          //           child: CircularProgressIndicator(),
+          //         );
+          //       }
+          //     })
+
+          // Container(
+          //     padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          //     height: 100,
+          //     child: Card(
+          //         child: FutureBuilder(
+          //           future: Upload(imageFile!),
+          //           builder: (context, snapshot) {
+          //             if (snapshot.data == null) {
+          //               return Container()
+          //             }
+          //
+          //           },
+          //         )
+          //     )
+          // )
+
+        );
+      },
+    );
+  }
+
+  void getImage({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(
+        source: source,
+        maxWidth: 300,
+        maxHeight: 300,
+        imageQuality: 70 //0 - 100
+    );
+
+    if (file?.path != null) {
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
+  }
+
+  Future Upload(File imageFile) async {
+    var headers = {
+      'Authorization': 'Bearer dc14a17ca4e34b79e0cf0773ac83df914e7e15f7'
+    };
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse(
+            'https://api.logmeal.es/v2/image/recognition/complete/v1.0?language=eng'));
+    request.files
+        .add(await http.MultipartFile.fromPath('image', imageFile.path));
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var s = (await response.stream.bytesToString());
+      var body = jsonDecode(s)['recognition_results'] as List;
+      var valuesApi = body.map((e) => e['name']).toList();
+      print(valuesApi);
+      void valuesApiPrinter() {
+        for (var listLogMeal in valuesApi) {
+          print(listLogMeal);
+        }
+      }
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
+  Widget Futur(BuildContext context) {
+    return FutureBuilder(
+        future: Upload(imageFile!),
+        builder: (context, snapshot) {
+          return Scaffold(
+            backgroundColor: Colors.green[50],
+            body:
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                children: [
+                  SizedBox(width: 20),
+                  if (snapshot.hasData) Text('${snapshot.data!} ХУЛИ НЕ РАБОТАЕМ АААААААААААА')
+                ],
+              ),
+            ]),
+          );
+        });
+  }
+}
